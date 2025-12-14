2fec86c763af25eb22283e41ef24bd7b8742b43c
Junaid <junaid8bp01@gmail.com>
2025-12-14 21:44:44 +0500

    Auto push JS updates

---
+++ DOM/2_DOM selector.js
@@ -0,0 +1,13 @@
+//Dom selector is defined as the method used to select and manipulate HTML elements in a web page using JavaScript.
+//There are several types of DOM selectors available in JavaScript, including:
+//1. getElementById: This selector is used to select an element based on its unique ID attribute.
+//2. getElementsByClassName: This selector is used to select elements based on their class attribute.   
+//3. getElementsByTagName: This selector is used to select elements based on their tag name.
+//4. querySelector: This selector is used to select the first element that matches a specified CSS selector.
+//5. querySelectorAll: This selector is used to select all elements that match a specified CSS selector.
+//Using these selectors, developers can easily manipulate the content and structure of a web page dynamically.
+//DOM manipulation is an essential part of web development as it allows developers to create dynamic and interactive web pages.
+//For example, we can use DOM to change the text of a paragraph, change the color of a div, add or remove elements from
+//the page and so on.
+//In summary, DOM selectors are powerful tools that allow developers to interact with and manipulate HTML elements
+//on a web page using JavaScript, making it possible to create dynamic and engaging user experiences.
